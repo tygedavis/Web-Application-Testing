396b79970f6832b456fdeac96b942496e53743bf
tygedavis <tyge.davis@gmail.com>
2019-12-05 16:15:34 -0700

    App functions properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-
+      <Dashboard />
     </div>
   );
 }
