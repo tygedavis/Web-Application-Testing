6d3f5817968ee36b8a4b310fe4b6c9b479af499d
tygedavis <tyge.davis@gmail.com>
2019-12-05 17:01:10 -0700

    App functions properly, no test have been added

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -8,21 +8,27 @@ const Dashboard = (props) => {
   const handleStrike = () => {
     if(strike < 3) {
       setStrike(strike + 1);
+    }else if (strike === 3) {
+      alert("You're out!");
+      setStrike(0);
+      setBalls(0);
     };
   };
 
     const handleBalls = () => {
     if(balls < 4) {
       setBalls(balls + 1);
-    };
+    }else if (balls === 4) {
+      alert("You walk!");
+      setStrike(0);
+      setBalls(0);
+    }
   };
 
   const handleFoul = () => {
     if(strike < 2) {
       setStrike(strike + 1);
-    }else{
-      setStrike(strike);
-    }
+    };
   };
 
   const handleHit = () => {
